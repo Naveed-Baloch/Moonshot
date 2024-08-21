0cb4de1ec12790b57e1e48c3fb83cdd731724249
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-21 19:09:29 +0500

    Initial Commit

---
+++ moonshot/moonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonshotApp.swift
+//  moonshot
+//
+//  Created by Naveed on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct moonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
